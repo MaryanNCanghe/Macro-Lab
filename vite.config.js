7c84bbef95bc14114919cb2e30952ab4f8dec941
Maria <mariancanghe@icloud.com>
2024-08-28 23:29:59 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Macro-Lab/', // Base URL for GitHub Pages
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   },
 });
